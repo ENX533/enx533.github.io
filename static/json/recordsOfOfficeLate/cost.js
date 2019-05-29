c5c46448546829c28430bca8adf66779a387bc3d
ENX533 <30405625+ENX533@users.noreply.github.com>
2019-05-29 10:00:02 +0800

    add some json

---
+++ static/json/recordsOfOfficeLate/cost.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    date: '2019-03-08',
+    data: [
+      {
+        amount: 25.5,
+        des: '下午茶 饮料'
+      },
+      {
+        amount: 368,
+        des: '下午茶 KFC'
+      }
+    ]
+  },
+  {
+    date: '2019-04-11',
+    data: [
+      {
+        amount: 560,
+        des: '晚餐 酸菜鱼'
+      }
+    ]
+  },
+  {
+    date: '2019-04-24',
+    data: [
+      {
+        amount: 252.6,
+        des: '下午茶 迁徙前的归零'
+      }
+    ]
+  }
+]
---
+++ static/json/recordsOfOfficeLate/record.js
@@ -0,0 +1,946 @@
+export default [
+  {
+    date: '2019-01-18',
+    data: [
+      {
+        person: '麦少英',
+        time: 4,
+        amount: 8
+      }
+    ]
+  },
+  {
+    date: '2019-01-21',
+    data: [
+      {
+        person: '麦少英',
+        time: 4,
+        amount: 8
+      },
+      {
+        person: '陈恒',
+        time: 4,
+        amount: 8
+      }
+    ]
+  },
+  {
+    date: '2019-01-22',
+    data: [
+      {
+        person: '陈国华',
+        time: 2,
+        amount: 4
+      }
+    ]
+  },
+  {
+    date: '2019-01-23',
+    data: [
+      {
+        person: '庄翠萍',
+        time: 1,
+        amount: 2
+      },
+      {
+        person: '梁福志',
+        time: 1,
+        amount: 2
+      },
+      {
+        person: '陈恒',
+        time: 8,
+        amount: 16
+      }
+    ]
+  },
+  {
+    date: '2019-01-24'
+  },
+  {
+    date: '2019-01-25',
+    data: [
+      {
+        person: '陈国华',
+        time: 1,
+        amount: 2
+      },
+      {
+        person: '廖文超',
+        time: 4,
+        amount: 8
+      },
+      {
+        person: '陈恒',
+        time: 4,
+        amount: 8
+      }
+    ]
+  },
+  {
+    date: '2019-01-27',
+    data: [
+      {
+        person: '陈国华',
+        time: 5,
+        amount: 10
+      },
+      {
+        person: '梁福志',
+        time: 7,
+        amount: 14
+      }
+    ]
+  },
+  {
+    date: '2019-01-28',
+    data: [
+      {
+        person: '钟卫芳',
+        time: 1,
+        amount: 2
+      },
+      {
+        person: '梁福志',
+        time: 5,
+        amount: 10
+      },
+      {
+        person: '陈恒',
+        time: 6,
+        amount: 12
+      }
+    ]
+  },
+  {
+    date: '2019-01-29',
+    data: [
+      {
+        person: '陈恒',
+        time: 1,
+        amount: 3
+      }
+    ]
+  },
+  {
+    date: '2019-01-30'
+  },
+  {
+    date: '2019-01-31',
+    data: [
+      {
+        person: '麦少英',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '陈恒',
+        time: 3,
+        amount: 9
+      }
+    ]
+  },
+  {
+    date: '2019-02-01'
+  },
+  {
+    date: '2019-02-11'
+  },
+  {
+    date: '2019-02-12'
+  },
+  {
+    date: '2019-02-13',
+    data: [
+      {
+        person: '庄翠萍',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '钟卫芳',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '陈恒',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '林锦坝',
+        time: 6,
+        amount: 18
+      }
+    ]
+  },
+  {
+    date: '2019-02-14'
+  },
+  {
+    date: '2019-02-15'
+  },
+  {
+    date: '2019-02-16',
+    data: [
+      {
+        person: '陈幼梅',
+        time: 7,
+        amount: 21
+      }
+    ]
+  },
+  {
+    date: '2019-02-18',
+    data: [
+      {
+        person: '陈思雨',
+        time: 16,
+        amount: 48
+      }
+    ]
+  },
+  {
+    date: '2019-02-19',
+    data: [
+      {
+        person: '陈国华',
+        time: 1,
+        amount: 3.1
+      }
+    ]
+  },
+  {
+    date: '2019-02-20',
+    data: [
+      {
+        person: '麦少英',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '陈幼梅',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '廖文超',
+        time: 3,
+        amount: 9
+      }
+    ]
+  },
+  {
+    date: '2019-02-21',
+    data: [
+      {
+        person: '钟卫芳',
+        time: 3,
+        amount: 9
+      }
+    ]
+  },
+  {
+    date: '2019-02-22',
+    data: [
+      {
+        person: '陈国华',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '陈幼梅',
+        time: 1,
+        amount: 3
+      }
+    ]
+  },
+  {
+    date: '2019-02-25',
+    data: [
+      {
+        person: '廖文超',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '陈国华',
+        time: 6,
+        amount: 18
+      }
+    ]
+  },
+  {
+    date: '2019-02-26',
+    data: [
+      {
+        person: '麦少英',
+        time: 2,
+        amount: 6
+      }
+    ]
+  },
+  {
+    date: '2019-02-27',
+    data: [
+      {
+        person: '陈思雨',
+        time: 2,
+        amount: 6
+      },
+      {
+        person: '陈恒',
+        time: 8,
+        amount: 24
+      },
+      {
+        person: '陈国华',
+        time: 11,
+        amount: 33
+      }
+    ]
+  },
+  {
+    date: '2019-02-28',
+    data: [
+      {
+        person: '陈恒',
+        time: 3,
+        amount: 9
+      }
+    ]
+  },
+  {
+    date: '2019-03-01',
+    data: [
+      {
+        person: '陈恒',
+        time: 3,
+        amount: 9
+      }
+    ]
+  },
+  {
+    date: '2019-03-04',
+    data: [
+      {
+        person: '廖文超',
+        time: 11,
+        amount: 33
+      }
+    ]
+  },
+  {
+    date: '2019-03-05',
+    data: [
+      {
+        person: '陈恒',
+        time: 2,
+        amount: 6
+      }
+    ]
+  },
+  {
+    date: '2019-03-06',
+    data: [
+      {
+        person: '陈思雨',
+        time: 8,
+        amount: 24
+      },
+      {
+        person: '陈恒',
+        time: 8,
+        amount: 24
+      }
+    ]
+  },
+  {
+    date: '2019-03-07',
+    data: [
+      {
+        person: '麦少英',
+        time: 2,
+        amount: 6
+      },
+      {
+        person: '陈恒',
+        time: 2,
+        amount: 6
+      }
+    ]
+  },
+  {
+    date: '2019-03-08',
+    data: [
+      {
+        person: '陈恒',
+        time: 4,
+        amount: 12
+      }
+    ]
+  },
+  {
+    date: '2019-03-11',
+    data: [
+      {
+        person: '陈恒',
+        time: 2,
+        amount: 6
+      },
+      {
+        person: '梁福志',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '麦少英',
+        time: 20,
+        amount: 60
+      }
+    ]
+  },
+  {
+    date: '2019-03-12',
+    data: [
+      {
+        person: '庄翠萍',
+        time: 1,
+        amount: 3
+      }
+    ]
+  },
+  {
+    date: '2019-03-14',
+    data: [
+      {
+        person: '陈恒',
+        time: 11,
+        amount: 33
+      }
+    ]
+  },
+  {
+    date: '2019-03-15',
+    data: [
+      {
+        person: '陈思雨',
+        time: 10,
+        amount: 30
+      },
+      {
+        person: '陈恒',
+        time: 10,
+        amount: 30
+      }
+    ]
+  },
+  {
+    date: '2019-03-18'
+  },
+  {
+    date: '2019-03-19',
+    data: [
+      {
+        person: '陈恒',
+        time: 6,
+        amount: 18
+      }
+    ]
+  },
+  {
+    date: '2019-03-20'
+  },
+  {
+    date: '2019-03-21',
+    data: [
+      {
+        person: '陈恒',
+        time: 8,
+        amount: 24
+      }
+    ]
+  },
+  {
+    date: '2019-03-22',
+    data: [
+      {
+        person: '梁福志',
+        time: 5,
+        amount: 15
+      }
+    ]
+  },
+  {
+    date: '2019-03-25',
+    data: [
+      {
+        person: '梁福志',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '廖文超',
+        time: 6,
+        amount: 18
+      },
+      {
+        person: '陆丽纯',
+        time: 0,
+        amount: 0
+      }
+    ]
+  },
+  {
+    date: '2019-03-26',
+    data: [
+      {
+        person: '陈恒',
+        time: 1,
+        amount: 3
+      }
+    ]
+  },
+  {
+    date: '2019-03-27'
+  },
+  {
+    date: '2019-03-28',
+    data: [
+      {
+        person: '魏静兰',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '陈恒',
+        time: 10,
+        amount: 30
+      }
+    ]
+  },
+  {
+    date: '2019-03-29'
+  },
+  {
+    date: '2019-04-01',
+    data: [
+      {
+        person: '陈恒',
+        time: 3,
+        amount: 9
+      },
+      {
+        person: '麦少英',
+        time: 7,
+        amount: 21
+      }
+    ]
+  },
+  {
+    date: '2019-04-02'
+  },
+  {
+    date: '2019-04-03',
+    data: [
+      {
+        person: '梁福志',
+        time: 3,
+        amount: 9
+      }
+    ]
+  },
+  {
+    date: '2019-04-04'
+  },
+  {
+    date: '2019-04-08',
+    data: [
+      {
+        person: '庄翠萍',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '梁福志',
+        time: 4,
+        amount: 12
+      }
+    ]
+  },
+  {
+    date: '2019-04-09',
+    data: [
+      {
+        person: '陈恒',
+        time: 2,
+        amount: 6
+      }
+    ]
+  },
+  {
+    date: '2019-04-10',
+    data: [
+      {
+        person: '马泽鑫',
+        time: 2,
+        amount: 6
+      },
+      {
+        person: '麦少英',
+        time: 8,
+        amount: 24
+      }
+    ]
+  },
+  {
+    date: '2019-04-11',
+    data: [
+      {
+        person: '陈思雨',
+        time: 5,
+        amount: 15
+      },
+      {
+        person: '陈恒',
+        time: 7,
+        amount: 21
+      }
+    ]
+  },
+  {
+    date: '2019-04-12',
+    data: [
+      {
+        person: '庄翠萍',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '陈恒',
+        time: 7,
+        amount: 21
+      }
+    ]
+  },
+  {
+    date: '2019-04-15',
+    data: [
+      {
+        person: '陈恒',
+        time: 7,
+        amount: 21
+      },
+      {
+        person: '陈思雨',
+        time: 11,
+        amount: 33
+      },
+      {
+        person: '麦少英',
+        time: 11,
+        amount: 33
+      }
+    ]
+  },
+  {
+    date: '2019-04-16',
+    data: [
+      {
+        person: '梁福志',
+        time: 2,
+        amount: 6
+      }
+    ]
+  },
+  {
+    date: '2019-04-17',
+    data: [
+      {
+        person: '陈恒',
+        time: 13,
+        amount: 39
+      }
+    ]
+  },
+  {
+    date: '2019-04-18',
+    data: [
+      {
+        person: '麦少英',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '陈恒',
+        time: 17,
+        amount: 51
+      }
+    ]
+  },
+  {
+    date: '2019-04-19',
+    data: [
+      {
+        person: '廖文超',
+        time: 1,
+        amount: 3
+      },
+      {
+        person: '陈思雨',
+        time: 5,
+        amount: 15
+      },
+      {
+        person: '陈恒',
+        time: 12,
+        amount: 36
+      }
+    ]
+  },
+  {
+    date: '2019-04-22',
+    data: [
+      {
+        person: '陈恒',
+        time: 4,
+        amount: 12
+      }
+    ]
+  },
+  {
+    date: '2019-04-23'
+  },
+  {
+    date: '2019-04-24',
+    data: [
+      {
+        person: '陈幼梅',
+        time: 2,
+        amount: 6
+      }
+    ]
+  },
+  {
+    date: '2019-04-25'
+  },
+  {
+    date: '2019-04-26'
+  },
+  {
+    date: '2019-04-28'
+  },
+  {
+    date: '2019-04-29',
+    data: [
+      {
+        person: '陈恒',
+        time: 5,
+        amount: 15
+      }
+    ]
+  },
+  {
+    date: '2019-04-30',
+    data: [
+      {
+        person: '陈思雨',
+        time: 9,
+        amount: 27
+      },
+      {
+        person: '陈恒',
+        time: 15,
+        amount: 45
+      }
+    ]
+  },
+  {
+    date: '2019-05-05'
+  },
+  {
+    date: '2019-05-06',
+    data: [
+      {
+        person: '廖文超',
+        time: 4,
+        amount: 12
+      },
+      {
+        person: '陈恒',
+        time: 13,
+        amount: 39
+      }
+    ]
+  },
+  {
+    date: '2019-05-07',
+    data: [
+      {
+        person: '梁福志',
+        time: 2,
+        amount: 6
+      },
+      {
+        person: '陈恒',
+        time: 18,
+        amount: 54
+      }
+    ]
+  },
+  {
+    date: '2019-05-08',
+    data: [
+      {
+        person: '陈恒',
+        time: 12,
+        amount: 36
+      }
+    ]
+  },
+  {
+    date: '2019-05-09',
+    data: [
+      {
+        person: '陈恒',
+        time: 12,
+        amount: 36
+      }
+    ]
+  },
+  {
+    date: '2019-05-10'
+  },
+  {
+    date: '2019-05-13',
+    data: [
+      {
+        person: '麦少英',
+        time: 4,
+        amount: 12
+      },
+      {
+        person: '陈恒',
+        time: 7,
+        amount: 21
+      },
+      {
+        person: '廖文超',
+        time: 8,
+        amount: 24
+      }
+    ]
+  },
+  {
+    date: '2019-05-14',
+    data: [
+      {
+        person: '陈恒',
+        time: 13,
+        amount: 39
+      }
+    ]
+  },
+  {
+    date: '2019-05-15'
+  },
+  {
+    date: '2019-05-16',
+    data: [
+      {
+        person: '陈恒',
+        time: 2,
+        amount: 6
+      },
+      {
+        person: '麦少英',
+        time: 4,
+        amount: 12
+      }
+    ]
+  },
+  {
+    date: '2019-05-17'
+  },
+  {
+    date: '2019-05-20',
+    data: [
+      {
+        person: '陈恒',
+        time: 4,
+        amount: 12
+      }
+    ]
+  },
+  {
+    date: '2019-05-21'
+  },
+  {
+    date: '2019-05-22',
+    data: [
+      {
+        person: '麦少英',
+        time: 4,
+        amount: 12
+      },
+      {
+        person: '陈恒',
+        time: 5,
+        amount: 15
+      }
+    ]
+  },
+  {
+    date: '2019-05-23',
+    data: [
+      {
+        person: '陈恒',
+        time: 5,
+        amount: 15
+      }
+    ]
+  },
+  {
+    date: '2019-05-24'
+  },
+  {
+    date: '2019-05-27',
+    data: [
+      {
+        person: '廖文超',
+        time: 6,
+        amount: 18
+      },
+      {
+        person: '陈恒',
+        time: 10,
+        amount: 30
+      },
+      {
+        person: '陆丽纯',
+        time: 13,
+        amount: 39
+      }
+    ]
+  },
+  {
+    date: '2019-05-28',
+    data: [
+      {
+        person: '陈恒',
+        time: 14,
+        amount: 42
+      }
+    ]
+  },
+  {
+    date: '2019-05-29',
+    data: [
+      {
+        person: '陈恒',
+        time: 10,
+        amount: 30
+      }
+    ]
+  }
+]
